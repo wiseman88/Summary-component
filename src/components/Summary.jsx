3e575c47fc05d27b408b3c7a97b733f0a1316082
wiseman88 <wiseadam02@gmail.com>
2023-02-26 15:10:05 +0100

    clean up

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -12,46 +12,6 @@ const Summary = () => {
       {data.map((block, index) => (
         <Block key={index} block={block} />
       ))}
-      {/* <div className={`${summary.block} ${summary["error-block"]}`}>
-        <div className={summary.title}>
-          <img src="/images/icon-reaction.svg" alt="reaction" />
-          <span>Reaction</span>
-        </div>
-        <span className={summary.points}>
-          <span className={summary.total}>80</span> / 100
-        </span>
-      </div>
-
-      <div className={`${summary.block} ${summary["warning-block"]}`}>
-        <div className={summary.title}>
-          <img src="/images/icon-memory.svg" alt="memory" />
-          <span>Memory</span>
-        </div>
-        <span className={summary.points}>
-          <span className={summary.total}>92</span> / 100
-        </span>
-      </div>
-
-      <div className={`${summary.block} ${summary["success-block"]}`}>
-        <div className={summary.title}>
-          <img src="/images/icon-verbal.svg" alt="verbal" />
-          <span>Verbal</span>
-        </div>
-        <span className={summary.points}>
-          <span className={summary.total}>61</span> / 100
-        </span>
-      </div>
-
-      <div className={`${summary.block} ${summary["primary-block"]}`}>
-        <div className={summary.title}>
-          <img src="/images/icon-visual.svg" alt="visual" />
-          <span>Visual</span>
-        </div>
-        <span className={summary.points}>
-          <span className={summary.total}>72</span> / 100
-        </span>
-      </div> */}
-
       <button className={summary.btn}>Continue</button>
     </div>
   );
